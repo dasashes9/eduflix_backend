1d9eaf0760960cc148c4c17286be1e69557b72f8
Ashes Das <dasashes04@gmail.com>
2025-05-03 11:09:37 +0530

    added cors

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import UserRoute from "./routes/UserRoutes.js";
 import CourseRoutes from "./routes/CourseRoutes.js";
 import TutorRoutes from "./routes/TutorRoute.js"
 import dotenv from "dotenv";
-
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 connectDB();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user" , UserRoute);
